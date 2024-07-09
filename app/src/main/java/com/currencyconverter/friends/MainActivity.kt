2e05d7acd23ba978fa63e8bcc9feb746866e629a
aliCashGate <ali@cashgatetech.com>
2024-07-09 15:51:05 +0500

    Playstore resources updated

--- app/src/main/java/com/currencyconverter/friends/MainActivity.kt
+++ app/src/main/java/com/currencyconverter/friends/MainActivity.kt
@@ -3,12 +3,16 @@ package com.currencyconverter.friends
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.currencyconverter.friends.ui.theme.FriendsTheme
 
@@ -22,7 +26,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    Box(modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center){
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_launcher),
+                            contentDescription = null
+                        )
+                    }
+
+
+                    //Greeting("Android")
                 }
             }
         }
