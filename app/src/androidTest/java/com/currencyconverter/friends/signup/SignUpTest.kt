d817def09483c17e5d52a921ff8a3c3cffe38e90
aliCashGate <ali@cashgatetech.com>
2024-07-11 09:08:51 +0500

    Updated the strings resources

---
+++ app/src/androidTest/java/com/currencyconverter/friends/signup/SignUpTest.kt
@@ -0,0 +1,24 @@
+package com.currencyconverter.friends.signup
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.currencyconverter.friends.MainActivity
+import org.junit.Rule
+import org.junit.Test
+
+class SignUpTest {
+
+    @get:Rule
+    val signUpTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun performSignUp(){
+        launchSignUpScreen(signUpTestRule) {
+            typeEmail("email@test.com")
+            typePassword("password")
+            submit()
+        } verify {
+            timeLineScreenIsPresent()
+        }
+    }
+
+}
