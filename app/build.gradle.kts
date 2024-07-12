b6efb5701ab79617244a3ad497c082bbf6ca2557
aliCashGate <ali@cashgatetech.com>
2024-07-12 14:35:43 +0500

    Setup for JUnit5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ android {
 
 
     tasks.withType<Test> {
+        useJUnitPlatform()
         testLogging {
             events("passed", "failed", "skipped", "standardOut", "standardError")
         }
@@ -78,11 +79,14 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    testImplementation(libs.junit.api)
+    testImplementation(libs.junit.params)
+    testRuntimeOnly(libs.junit)
 }
